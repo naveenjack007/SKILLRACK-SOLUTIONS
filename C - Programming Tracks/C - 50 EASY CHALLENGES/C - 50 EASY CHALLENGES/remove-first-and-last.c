2796690b748bab0abcec809ce8fec1a28276a415
Naveen Natarajan <58937732+naveenjack007@users.noreply.github.com>
2021-05-04 10:22:19 +0530

    Create remove-first-and-last.c

---
+++ C - Programming Tracks/C - 50 EASY CHALLENGES/C - 50 EASY CHALLENGES/remove-first-and-last.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    int n,i=0,j;
+    char str[200];
+    scanf("%s",str);
+    n=strlen(a);
+    for(j=i+1;j<n-1-i;j++){
+        printf("%c",a[j]);
+    }
+
+}
